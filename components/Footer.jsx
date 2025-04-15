230aaa76aa6141a8b4fedd68e607a9666dc3a7cf
niranjana <niranjanasiju92@gmail.com>
2025-04-15 16:35:52 +0530

    added gradient to the grids in footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="w-full bg-grid flex flex-col items-center justify-center text-white relative overflow-hidden">
+    <footer className="relative w-full flex flex-col items-center justify-center text-white before:absolute before:inset-0 before:z-[-1] before:[background-image:repeating-linear-gradient(to_bottom,rgba(255,255,255,0.03)_0px,rgba(255,255,255,0.03)_1px,transparent_1px,transparent_100px),repeating-linear-gradient(to_right,rgba(255,255,255,0.03)_0px,rgba(255,255,255,0.03)_1px,transparent_1px,transparent_100px)] before:[mask-image:linear-gradient(to_bottom,transparent_0%,black_40%,black_80%,transparent_100%)] before:content-[''] before:pointer-events-none">
       
       {/* Main Call-to-Action Section */}
       <div className="min-h-[calc(100vh-250px)] w-full flex flex-col items-center justify-center text-center pt-16">
@@ -19,7 +19,7 @@ const Footer = () => {
 
       {/* Bottom Footer Bar */}
       <div className="w-full h-[130px] relative z-10">
-        <div className="absolute inset-0 backdrop-blur-sm bg-white/2 border-t border-t-[#FFFFFF29]" />
+        <div className="absolute inset-0 backdrop-blur-lgs bg-white/2.5 border-t border-t-[#FFFFFF29]" />
         <div className="relative z-20 max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center text-sm text-gray-500 h-full px-6">
           <p className="mb-4 md:mb-0 font-mono">FOSS MEC – We build. We share. We learn.</p>
           <div className="flex space-x-6 font-mono">

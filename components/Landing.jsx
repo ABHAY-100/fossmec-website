aec557bf2bd40a1571c3537672062eb011a77f29
Midhun Unni <midhununni457@gmail.com>
2025-04-16 19:46:22 +0530

    landing done

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -6,20 +6,25 @@ const Landing = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center relative">
       <div className="absolute top-0 left-0 inset-0 z-[-1] overflow-hidden">
-        <Image src={landingBg} alt="Background image" fill />
+        <Image
+          src={landingBg}
+          alt="Background image"
+          fill
+          className="object-cover"
+        />
       </div>
 
-      <div className="z-10 max-w-[80%] w-full px-4 flex flex-col items-start">
+      <div className="z-10 max-w-[85%] w-full px-4 flex flex-col items-start">
         <div className="flex flex-col items-start">
-          <h1 className="text-4xl md:text-5xl lg:text-7xl xl:text-[95px] font-semibold leading-[120px]">
+          <h1 className="text-4xl md:text-5xl lg:text-7xl xl:text-[100px] font-uncut-sans font-extrabold leading-[120px]">
             OPEN SOURCE
           </h1>
-          <h1 className="text-4xl md:text-5xl lg:text-7xl xl:text-[95px] font-semibold leading-none mt-[-0.1em]">
+          <h1 className="text-4xl md:text-5xl lg:text-7xl xl:text-[100px] font-uncut-sans font-extrabold leading-none mt-[-0.1em]">
             SOFTWARE FOUNDATION
           </h1>
         </div>
 
-        <p className="text-sm md:text-base lg:text-lg xl:text-xl mt-3 leading-[38px] max-w-9/12 uppercase text-[#DAE2E9E0] opacity-88">
+        <p className="text-sm md:text-sm lg:text-md xl:text-[18px] font-dm-mono italic mt-5 leading-[32px] max-w-9/12 uppercase text-[#DAE2E9E0] opacity-88">
           FOSS MEC IS A NON-PROFIT FOUNDATION THAT AIMS AT PROMOTING AND
           STRENGTHENING THE FREE AND OPEN SOURCE SOFTWARE (FOSS) ECOSYSTEM AMONG
           MECIANS

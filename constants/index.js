505485682291cd1c2a2cf1030d6595bad8c7d555
1337kid <45532566+1337kid@users.noreply.github.com>
2025-05-31 12:02:12 +0530

    add prev yr teams

--- constants/index.js
+++ constants/index.js
@@ -1,179 +1,420 @@
-import aadithya from "@/assets/Team/Aadithya_Madhav__Executive.png";
-import jeswin from "@/assets/Team/Jeswin_Joseph_Treasurer.png";
-import pranav from "@/assets/Team/Pranav_P_S__Operations_Lead.png";
-import adhish from "@/assets/Team/Adhish_Joseph_Shinu_Sub-Marketing_Lead.png";
-import karthik from "@/assets/Team/Karthik_G_Kumar__AI_ML_Lead.png";
-import roshin from "@/assets/Team/Roshin_Jimmy_Joint_Secretary.png";
-import alan from "@/assets/Team/Alan_Biju__Open_Hardware_Advocate.png";
-import khushi from "@/assets/Team/Khushi_Ved__Tech_Lead.png";
-import sheril from "@/assets/Team/Sheril_Dominic__Events_sub_head.png";
-import anavadya from "@/assets/Team/Anavadya_N_Lakshmi__Design_Sub_Lead.png";
-import varun from "@/assets/Team/KV_Varun_Krishnan_Vice_Chairperson.png";
-import sreepria from "@/assets/Team/Sreepria_KM_Executive.png";
-import arjun from "@/assets/Team/Arjun_C_S__Marketing_head.png";
-import yasir from "@/assets/Team/Mohammed_Yasir_K_N__Executive.png";
-import srilakshmi from "@/assets/Team/Srilakshmi_R_Content_Head.png";
-import theertha from "@/assets/Team/A_Theertha__Secretary.png";
-import nikesh from "@/assets/Team/Nikesh_T_V__Events_Lead.png";
-import vasanth from "@/assets/Team/Vasanth_R__Executive.png";
-import deepak from "@/assets/Team/Deepak_P_Nair_Content_Sub-Head.png";
-import nikhil from "@/assets/Team/Nikhil_M__Chairperson.png";
-import jaimy from "@/assets/Team/Jaimy_Renji__Design_Lead.png";
-import joel from "@/assets/Team/Joel_Roshan__Web_Lead.png";
+import AadithyaMadhav from "@/assets/Team/AadithyaMadhav.png";
+import AdhishJosephShinu from "@/assets/Team/AdhishJosephShinu.jpg";
+import AlanBiju from "@/assets/Team/AlanBiju.jpg";
+import AlakaAJ from "@/assets/Team/AlakaAJ.jpg";
+import AlfredJimmy from "@/assets/Team/AlfredJimmy.jpeg";
+import AnakhaSKumar from "@/assets/Team/AnakhaSKumar.jpg";
+import AnavadyaNLakshmi from "@/assets/Team/AnavadyaNLakshmi.jpg";
+import AnnuKuruvilla from "@/assets/Team/AnnuKuruvilla.jpg";
+import ArjunCS from "@/assets/Team/ArjunCS.jpg";
+import DeepakPNair from "@/assets/Team/DeepakPNair.JPG";
+import JaimyRenji from "@/assets/Team/JaimyRenji.jpg";
+import JeswinJ from "@/assets/Team/JeswinJ.jpeg";
+import JithinJagadeesh from "@/assets/Team/JithinJagadeesh.jpg";
+import JozefAntonyNeelamkavil from "@/assets/Team/JozefAntonyNeelamkavil.jpg";
+import KarthikGKumar from "@/assets/Team/KarthikGKumar.jpg";
+import KhushiVed from "@/assets/Team/KhushiVed.jpg";
+import KVVarunKrishnan from "@/assets/Team/KVVarunKrishnan.jpeg";
+import LisaVC from "@/assets/Team/LisaVC.jpeg";
+import MekhaL from "@/assets/Team/MekhaL.jpeg";
+import MohdYasirKN from "@/assets/Team/MohdYasirKN.jpg";
+import NikhilM from "@/assets/Team/NikhilM.jpg";
+import NikeshTV from "@/assets/Team/NikeshTV.png";
+import NiyaShiyas from "@/assets/Team/NiyaShiyas.jpg";
+import PranavPS from "@/assets/Team/PranavPS.jpg";
+import RahulVS from "@/assets/Team/RahulVS.jpg";
+import RoshinJimmy from "@/assets/Team/RoshinJimmy.jpg";
+import SherilDominic from "@/assets/Team/SherilDominic.jpg";
+import SreepriyaKM from "@/assets/Team/SreepriyaKM.jpg";
+import SrilakshmiRanganath from "@/assets/Team/SrilakshmiRanganath.jpg";
+import TheerthaAvinash from "@/assets/Team/TheerthaAvinash.JPG";
+import VishnuRPotti from "@/assets/Team/VishnuRPotti.jpg";
+import VasanthR from "@/assets/Team/VasanthR.jpeg";
+import JoelRoshan from "@/assets/Team/JoelRoshan.png";
 
-export const TEAM = [
+export const TEAM2024 = [
   {
     id: 1,
     value: "Item 1",
     name: "Nikhil",
     position: "Chair",
-    img: nikhil,
+    img: NikhilM,
   },
   {
     id: 2,
     value: "Item 2",
     name: "Varun",
     position: "Vice Chair",
-    img: varun,
+    img: KVVarunKrishnan,
   },
   {
     id: 3,
     value: "Item 3",
     name: "Theertha",
     position: "Secretary",
-    img: theertha,
+    img: TheerthaAvinash,
   },
   {
     id: 4,
     value: "Item 4",
     name: "Roshin",
     position: "Joint Secretary",
-    img: roshin,
+    img: RoshinJimmy,
   },
   {
     id: 5,
     value: "Item 5",
     name: "Alan",
     position: "Open Hardware Advocate",
-    img: alan,
+    img: AlanBiju,
   },
   {
     id: 6,
     value: "Item 6",
     name: "Khushi",
     position: "Tech Lead",
-    img: khushi,
+    img: KhushiVed,
   },
   {
     id: 7,
     value: "Item 7",
     name: "Jeswin",
     position: "Treasurer",
-    img: jeswin,
+    img: JeswinJ,
   },
   {
     id: 8,
     value: "Item 8",
     name: "Pranav",
     position: "Operations Lead",
-    img: pranav,
+    img: PranavPS,
   },
   {
     id: 9,
     value: "Item 9",
     name: "Karthik",
     position: "AI/ML Lead",
-    img: karthik,
+    img: KarthikGKumar,
   },
   {
     id: 10,
     value: "Item 10",
     name: "Nikesh",
     position: "Events Lead",
-    img: nikesh,
+    img: NikeshTV,
   },
   {
     id: 11,
     value: "Item 11",
     name: "Sheril",
     position: "Events Sub-Head",
-    img: sheril,
+    img: SherilDominic,
   },
   {
     id: 12,
     value: "Item 12",
     name: "Jaimy",
     position: "Design Lead",
-    img: jaimy,
+    img: JaimyRenji,
   },
   {
     id: 13,
     value: "Item 13",
     name: "Anavadya",
     position: "Design Sub-Lead",
-    img: anavadya,
+    img: AnavadyaNLakshmi,
   },
   {
     id: 14,
     value: "Item 14",
     name: "Arjun",
     position: "Marketing Head",
-    img: arjun,
+    img: ArjunCS,
   },
   {
     id: 15,
     value: "Item 15",
     name: "Adhish",
     position: "Sub-Marketing Lead",
-    img: adhish,
+    img: AdhishJosephShinu,
   },
   {
     id: 16,
     value: "Item 16",
     name: "Srilakshmi",
     position: "Content Head",
-    img: srilakshmi,
+    img: SrilakshmiRanganath,
   },
   {
     id: 17,
     value: "Item 17",
     name: "Deepak",
     position: "Content Sub-Head",
-    img: deepak,
+    img: DeepakPNair,
   },
   {
     id: 18,
     value: "Item 18",
     name: "Joel",
     position: "Web Lead",
-    img: joel,
+    img: JoelRoshan,
   },
   {
     id: 19,
     value: "Item 19",
     name: "Aadithya",
     position: "Executive",
-    img: aadithya,
+    img: AadithyaMadhav,
   },
   {
     id: 20,
     value: "Item 20",
     name: "Yasir",
     position: "Executive",
-    img: yasir,
+    img: MohdYasirKN,
   },
   {
     id: 21,
     value: "Item 21",
     name: "Sreepria",
     position: "Executive",
-    img: sreepria,
+    img: SreepriyaKM,
   },
   {
     id: 22,
     value: "Item 22",
     name: "Vasanth",
     position: "Executive",
-    img: vasanth,
+    img: VasanthR,
+  },
+];
+
+export const TEAM2023 = [
+  {
+    id: 1,
+    value: "Item 1",
+    name: "Jithin",
+    position: "Chair",
+    img: JithinJagadeesh,
+  },
+  {
+    id: 2,
+    value: "Item 2",
+    name: "Vishnu",
+    position: "Vice Chair",
+    img: VishnuRPotti,
+  },
+  {
+    id: 3,
+    value: "Item 3",
+    name: "Karthik",
+    position: "Secretary",
+    img: KarthikGKumar,
+  },
+  {
+    id: 4,
+    value: "Item 4",
+    name: "Lisa",
+    position: "GitHub Campus Expert",
+    img: LisaVC,
+  },
+  {
+    id: 5,
+    value: "Item 5",
+    name: "Rahul",
+    position: "Treasurer",
+    img: RahulVS,
+  },
+  {
+    id: 6,
+    value: "Item 6",
+    name: "Srilakshmi",
+    position: "Events Head",
+    img: SrilakshmiRanganath,
+  },
+  {
+    id: 7,
+    value: "Item 7",
+    name: "Annu",
+    position: "Design Head",
+    img: AnnuKuruvilla,
+  },
+  {
+    id: 8,
+    value: "Item 8",
+    name: "Anakha",
+    position: "Content Head",
+    img: AnakhaSKumar,
+  },
+  {
+    id: 9,
+    value: "Item 9",
+    name: "Mekha",
+    position: "Marketing Head",
+    img: MekhaL,
+  },
+  {
+    id: 10,
+    value: "Item 10",
+    name: "Theertha",
+    position: "GSOC Committee Lead",
+    img: TheerthaAvinash,
+  },
+  {
+    id: 11,
+    value: "Item 11",
+    name: "Jozef",
+    position: "Tech Head",
+    img: JozefAntonyNeelamkavil,
+  },
+  {
+    id: 12,
+    value: "Item 12",
+    name: "Alan",
+    position: "AI/ML Head",
+    img: AlanBiju,
+  },
+  {
+    id: 13,
+    value: "Item 13",
+    name: "Alaka",
+    position: "UI/UX Head",
+    img: AlakaAJ,
+  },
+  {
+    id: 14,
+    value: "Item 14",
+    name: "Niya",
+    position: "Web Head",
+    img: NiyaShiyas,
+  },
+  {
+    id: 15,
+    value: "Item 15",
+    name: "Alfred",
+    position: "App Head",
+    img: AlfredJimmy,
+  },
+  {
+    id: 16,
+    value: "Item 16",
+    name: "Khushi",
+    position: "Executive",
+    img: KhushiVed,
+  },
+  {
+    id: 17,
+    value: "Item 17",
+    name: "Arjun",
+    position: "Executive",
+    img: ArjunCS,
+  },
+  {
+    id: 18,
+    value: "Item 18",
+    name: "Roshin",
+    position: "Executive",
+    img: RoshinJimmy,
+  },
+  {
+    id: 19,
+    value: "Item 19",
+    name: "Nikhil",
+    position: "Executive",
+    img: NikhilM,
+  },
+  {
+    id: 20,
+    value: "Item 20",
+    name: "Nikesh",
+    position: "Executive",
+    img: NikeshTV,
+  },
+];
+
+export const TEAM2022 = [
+  {
+    id: 1,
+    value: "Item 1",
+    name: "Aneeta",
+    position: "Chair",
+  },
+  {
+    id: 2,
+    value: "Item 2",
+    name: "Elizabeth",
+    position: "Vice Chair",
+  },
+  {
+    id: 3,
+    value: "Item 3",
+    name: "John",
+    position: "Inclusivity Lead",
+  },
+  {
+    id: 4,
+    value: "Item 4",
+    name: "Alfred",
+    position: "Community Lead",
+  },
+  {
+    id: 5,
+    value: "Item 5",
+    name: "Mayon",
+    position: "Secretary",
+  },
+  {
+    id: 6,
+    value: "Item 6",
+    name: "Jithin",
+    position: "Joint Secretary",
+  },
+  {
+    id: 7,
+    value: "Item 7",
+    name: "Adam",
+    position: "Tech Lead",
+  },
+  {
+    id: 8,
+    value: "Item 8",
+    name: "Salman",
+    position: "Tech Sub-Lead",
+  },
+  {
+    id: 9,
+    value: "Item 9",
+    name: "Jeswin",
+    position: "Design Lead",
+  },
+  {
+    id: 10,
+    value: "Item 10",
+    name: "Sony",
+    position: "Content Lead",
+  },
+  {
+    id: 11,
+    value: "Item 11",
+    name: "Parvathy",
+    position: "Executive",
+  },
+  {
+    id: 12,
+    value: "Item 12",
+    name: "Karthik",
+    position: "Executive",
+  },
+  {
+    id: 13,
+    value: "Item 13",
+    name: "Sreearvathy",
+    position: "Executive",
+  },
+  {
+    id: 14,
+    value: "Item 14",
+    name: "Subramani",
+    position: "Executive",
   },
 ];

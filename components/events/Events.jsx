3d6d6eb4bac6d541dfb4c5df92a2d5b3b6f2d25c
1337kid <45532566+1337kid@users.noreply.github.com>
2025-06-18 10:38:59 +0530

    minor update

--- components/events/Events.jsx
+++ components/events/Events.jsx
@@ -132,7 +132,7 @@ const Events = () => {
     >
       <div className="max-w-7xl mx-auto relative z-10 flex flex-col h-full py-8">
         <h2 className="text-3xl md:text-4xl font-semibold italic uppercase text-left px-2 sm:px-16 lg:px-28 mt-25 mb-6 font-uncut-sans tracking-tight leading-none bg-gradient-to-r from-[#C0AE42] via-[#379CA2] to-[#2C7FDC] bg-clip-text text-transparent">
-          &lt;What abt events?&gt;
+          &lt;WHAT ABOUT EVENTS?&gt;
         </h2>
 
         <p className="font-dm-mono text-white text-lg md:text-2xl mx-auto mb-8 font-normal tracking-tight leading-[120%] max-w-[1000px] mt-10 px-4 text-left">
